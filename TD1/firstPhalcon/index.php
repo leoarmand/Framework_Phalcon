3165641e05b222ab9a8403eaf0fdb98d3cd70e85
leoarmand <leoarmand@orange.fr>
2016-02-05 10:52:28 +0100

    commit atp

--- TD1/firstPhalcon/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leo
- * Date: 05/02/16
- * Time: 10:29
- */
--- TD1/firstPhalcon/public/index.php
+++ TD1/firstPhalcon/public/index.php
@@ -1,40 +1,36 @@
 <?php
 
-error_reporting(E_ALL);
-
-define('APP_PATH', realpath('..'));
-
 try {
- 
-    //Enregistrement d'un autoloader pour premettre l'inclusion auto des fichiers de classe
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
- 
-    //DI est le service responsable de l'injection de dépendance des services Phalcon utilisés
-    $di = new Phalcon\DI\FactoryDefault();
- 
-    //Configuration des vues
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
- 
-    //Configuration de l'URL de base
-    $di->set('url', function(){
-        $url = new \Phalcon\Mvc\Url();
-        $url->setBaseUri('/firstPhalcon/');
-        return $url;
-    });
- 
-    //Interception de la requête pour routage et dispatching
-    $application = new \Phalcon\Mvc\Application($di);
- 
-    echo $application->handle()->getContent();
- 
+
+//Enregistrement d'un autoloader pour premettre l'inclusion auto des fichiers de classe
+$loader = new \Phalcon\Loader();
+$loader->registerDirs(array(
+'../app/controllers/',
+'../app/models/'
+))->register();
+
+//DI est le service responsable de l'injection de dépendance des services Phalcon utilisés
+$di = new Phalcon\DI\FactoryDefault();
+
+//Configuration des vues
+$di->set('view', function(){
+$view = new \Phalcon\Mvc\View();
+$view->setViewsDir('../app/views/');
+return $view;
+});
+
+//Configuration de l'URL de base
+$di->set('url', function(){
+$url = new \Phalcon\Mvc\Url();
+$url->setBaseUri('/TD1/firstPhalcon/');
+return $url;
+});
+
+//Interception de la requête pour routage et dispatching
+$application = new \Phalcon\Mvc\Application($di);
+
+echo $application->handle()->getContent();
+
 } catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
+echo "PhalconException: ", $e->getMessage();
 }
