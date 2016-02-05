096c1bc454f7d84d0a53f1bad77da5bad0a4161b
leoarmand <leoarmand@orange.fr>
2016-02-05 11:17:57 +0100

    commit atp

--- TD1/atp/app/controllers/JoueurController.php
+++ TD1/atp/app/controllers/JoueurController.php
@@ -3,10 +3,28 @@
 class JoueurController extends \Phalcon\Mvc\Controller
 {
 
-    public function indexAction()
+    public function indexAction(sField="nom",sens="asc",filter=NULL)
     {
 
     }
 
-}
+    public function formAction(id=NULL)
+    {
 
+    }
+
+    public function updateAction(id=NULL)
+    {
+
+    }
+
+    public function deleteAction(id)
+    {
+
+    }
+
+    public function messageAction()
+    {
+
+    }
+}
