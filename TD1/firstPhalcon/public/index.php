18ac7801fb1f0ae2f94d97c8188af36db777630b
leoarmand <leoarmand@orange.fr>
2016-02-05 02:30:56 +0100

    first commit

---
+++ TD1/firstPhalcon/public/index.php
@@ -0,0 +1,40 @@
+<?php
+
+error_reporting(E_ALL);
+
+define('APP_PATH', realpath('..'));
+
+try {
+ 
+    //Enregistrement d'un autoloader pour premettre l'inclusion auto des fichiers de classe
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+ 
+    //DI est le service responsable de l'injection de dépendance des services Phalcon utilisés
+    $di = new Phalcon\DI\FactoryDefault();
+ 
+    //Configuration des vues
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    });
+ 
+    //Configuration de l'URL de base
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/firstPhalcon/');
+        return $url;
+    });
+ 
+    //Interception de la requête pour routage et dispatching
+    $application = new \Phalcon\Mvc\Application($di);
+ 
+    echo $application->handle()->getContent();
+ 
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
